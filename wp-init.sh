e6f8c32f6f6f09a42a49baf37e543a63032579d1
jerrycoe <jerrycoe2@gmail.com>
2018-04-04 06:16:36 -0700

    Updated bash script

--- wp-init.sh
+++ wp-init.sh
@@ -1,25 +1,34 @@
 #!/bin/bash
 
+#parse option
+while getopts ":r:" option; do
+	case ${option} in
+		r) 
+			remote=$OPTARG;;
+		:) 
+			echo "Invalid option";;
+	esac
+	shift $((OPTIND-1))
+done
+
 directory=$1
 theme="https://github.com/jerrycoe/wp-base-theme.git"
 
-while getopts r: option
-do
-case "${option}"
-in
-r) remote=${OPTARG};;
-esac
-done
+#make new directory for clone
+mkdir $directory
+cd $directory
 
-mkdir $DIRECTORY
-
-git clone $theme
- 
+#clone boilerplate repo
+git clone $theme .
 git init
-git add .
-git commit -m "Initial Commit"
 
-if [ -o remote ]; then
-	git remote add origin $remote
-	git push $remote master
-fi
+echo "Setting Remote"
+echo $remote
+
+#change remote url to user supplied repo
+git remote set-url origin $remote
+
+#add all files, commit and push
+git add -A
+git commit -m "Initial Commit"
+git push origin master
