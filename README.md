81e87d0fb726868a3297914470cd34d9a59446e9
jerrycoe <jerrycoe2@gmail.com>
2018-04-08 07:04:47 -0700

    using apache and mysql cli

--- README.md
+++ README.md
@@ -1,2 +1,62 @@
 # Bash-WP-Init
-Bash script to create new Wordpress Project and blank theme
+
+## What this does
+1. Downloads latest stable release of Wordpress
+2. Downloads boilerplate blank theme from my repo to themes directory
+3. Initializes a new git repo for blank theme
+4. Makes API call to Github to create new remote repo
+5. Pushes theme to new repo
+6. Creates new MySQL database
+7. Creates new host in /etc/hosts
+8. Creates new virtual host in /etc/apache2/extra/httpd-vhosts.conf
+9. Restarts apache2
+10. Opens new local website in browser
+</br>
+<p>This automates the process of creating a new local Wordpress Installation and theme git repo for development.</p>
+<p>My first bash script.</p>
+
+#### Notes
+<p>
+	<ul>
+		<li>
+			Apache
+			<li>
+				Requires unixd_module to be set with proper user and group in httpd.conf
+			</li>
+			<li>
+				Requires Include /private/etc/apache2/extra/httpd-vhosts.conf to be uncommented
+			</li>
+		</li>
+		<li>
+			MySQL
+			<li>
+				Requires valid credentials to create new databases
+			</li>
+		</li>
+		<li>
+			Script is configured to set up a new website in current working directory. This can be modified by adding another prompt at the beginning of the script.
+		</li>
+		<li>
+			 This script must be run under sudo for editing hosts and starting apache, as well as updating permissions.
+		</li>
+		<li>
+			Wordpress
+			<li>
+				Wordpress will have to finish installation in web browser to create wp-config.php with auth keys and database connection credentials.
+			</li>
+		</li>
+	</ul>
+
+</p>
+
+
+## Dependencies
+
+<ul>
+	<li>PHP CLI</li>
+	<li>Apache2</li>
+	<li>MySQL CLI</li>
+	<li>Git CLI</li>
+	<li>Git API OAuth Token</li>
+	<li>GNU sed</li>
+</ul>
--- wp-init.sh
+++ wp-init.sh
@@ -1,7 +1,10 @@
 #!/bin/bash
 #Set variables
-remote=$1
-directory=$1
+read -p "Name: " remote
+read -p "New Database Name: " dbName
+read -sp "MySQL password: " password
+directory=$remote
+nicename=$(echo "${remote}" | tr '[:upper:]' '[:lower:]')
 #JSON post data
 generate_post_data=$(cat <<-END
 {
@@ -24,7 +27,6 @@ remote="https://github.com/jerrycoe/WP-Theme-${directory}"
 #curl call Github API
 curl -s -H 'Content-Type: application/json' -u OUATHTOKEN:x-oauth-basic -d "$generate_post_data" "https://api.github.com/user/repos"
 
-
 #make new directory for clone
 mkdir $directory
 cd $directory
@@ -32,6 +34,7 @@ cd $directory
 echo "Cloning Wordpress latest stable release"
 echo $wp
 git clone $wp .
+
 #clone boilerplate repo
 cd "wp-content/themes/"
 mkdir "$directory-theme"
@@ -45,3 +48,34 @@ git remote set-url origin "${remote}"
 git add -A
 git commit -m "Initial Commit"
 git push origin master
+cd ..
+cd ..
+cd ..
+curdir=$(pwd)
+MYSQL_PWD="$password" mysql -u root -e "CREATE DATABASE $dbName;"
+
+#/etc/hosts
+cp /etc/hosts /etc/hosts.original
+echo -e "127.0.0.1\t${directory}.loc" >> /etc/hosts
+
+#httpd-vhosts.conf
+VHOSTSFILE="/etc/apache2/extra/httpd-vhosts.conf"
+cp $VHOSTSFILE ${VHOSTSFILE}.original
+echo "<VirtualHost *:80>" >> $VHOSTSFILE
+echo -e "\tDocumentRoot \"${curdir}\"" >> $VHOSTSFILE
+echo -e "\tServerName ${directory}.loc" >> $VHOSTSFILE
+echo -e "\tServerAlias www.${directory}.loc" >> $VHOSTSFILE
+echo -e "\t<Directory \"${curdir}\">" >> $VHOSTSFILE
+echo -e "\t\tDirectoryIndex index.html index.php" >> $VHOSTSFILE
+echo -e "\t\tOptions +Includes +Indexes +FollowSymLinks" >> $VHOSTSFILE
+echo -e "\t\tAllow from all" >> $VHOSTSFILE
+echo -e "\t\tRequire all granted" >> $VHOSTSFILE
+echo -e "\t\tAllowOverride all" >> $VHOSTSFILE
+echo -e "\t</Directory>" >> $VHOSTSFILE
+echo '</VirtualHost>' >> $VHOSTSFILE
+#allow writing to wp-config.php
+sudo chown -R jerrycoe:admin "${curdir}"
+#restart apache
+sudo apachectl restart
+#open project in web browser to complete wordpress install
+open http://"${directory}".loc
